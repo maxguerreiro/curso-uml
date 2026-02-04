ba9ef1a1118fd165fa2a9776e88ce974b5b27df4
Max Guerreiro <max.well.guerreiro12@gmail.com>
2026-02-04 13:40:27 -0300

    Classe Produto e associação 'many-to-many' com Categoria

--- src/main/java/com/maxguerreiro/cursouml/CursoumlApplication.java
+++ src/main/java/com/maxguerreiro/cursouml/CursoumlApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.maxguerreiro.cursouml.domain.Categoria;
+import com.maxguerreiro.cursouml.domain.Produto;
 import com.maxguerreiro.cursouml.repositories.CategoriaRepository;
+import com.maxguerreiro.cursouml.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoumlApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
@@ -26,10 +31,22 @@ public class CursoumlApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Consoles");
-		Categoria cat2 = new Categoria(null, "Gamer");
+		Categoria cat1 = new Categoria(null, "Games");
+		Categoria cat2 = new Categoria(null, "Console");
+		
+		Produto p1 = new Produto(null, "Computador", 6000.00);
+		Produto p2 = new Produto(null, "X-Box", 3000.00);
+		Produto p3 = new Produto(null, "Mouse", 120.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
