d53de1c249f8b7721c641b4dccb49c2b6dc94a98
Max Guerreiro <max.well.guerreiro12@gmail.com>
2026-02-04 12:19:32 -0300

    Projeto criado

---
+++ src/main/java/com/maxguerreiro/cursouml/CursoumlApplication.java
@@ -0,0 +1,13 @@
+package com.maxguerreiro.cursouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoumlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxguerreiro/cursouml/CursoumlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxguerreiro.cursouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoumlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
