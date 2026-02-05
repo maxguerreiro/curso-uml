a38e2348b91ae3bd50083fd9c45be97786820782
Max Guerreiro <max.well.guerreiro12@gmail.com>
2026-02-05 12:26:29 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/maxguerreiro/cursouml/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.maxguerreiro.cursouml.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maxguerreiro.cursouml.domain.Cliente;
+import com.maxguerreiro.cursouml.services.ClienteService;
+
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
